5ba6cbf6d6a7f575010eddfed837f0da590a3b0d
Manubilo <mjdelgado@pucp.pe>
2021-03-14 18:40:28 -0500

    added cors on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,7 @@ import microConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import cors from 'cors';
 
 import redis from 'redis';
 import session from 'express-session';
@@ -22,6 +23,12 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
 
   app.use(
     session({
@@ -50,7 +57,10 @@ const main = async () => {
     context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
     console.log('server started on localhost:4000');
